53333284b8a68cbaccadcbae46a80266fa2fdd5a
Laura Schörken <lauraschorken@gmail.com>
2025-08-15 16:19:36 +0200

    feat: constants file

---
+++ src/utils/constants.tsx
@@ -0,0 +1,9 @@
+export const appName = 'AppName'
+
+export type Lang = { code: string; label: string }
+export const AVAILABLE_LANGS: Lang[] = [
+	{ code: 'es', label: 'ES · Español' },
+	{ code: 'en', label: 'EN · English' },
+	// { code: 'de', label: 'DE · Deutsch' },
+	// { code: 'fr', label: 'FR · Français' },
+]
